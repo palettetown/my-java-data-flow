091c6de74ed2b54f0339ab4898b1231a00085c87
palettetown <kasinyee@outlook.com>
2024-04-14 12:33:16 -0400

    fix

--- my_script.sh
+++ my_script.sh
@@ -1,7 +1,7 @@
 gcloud dataflow flex-template build gs://bkt-tf-chrisyue3/df-tmpl-java.json \
- --image-gcr-path "us-central1-docker.pkg.dev/my-second-project-418213/my-docker-repo/df-tmpl-image:latest" \
+ --image-gcr-path "us-central1-docker.pkg.dev/my-second-project-418213/my-docker-repo/df-tmpl-image2:latest" \
  --sdk-language "JAVA" \
  --flex-template-base-image JAVA17 \
  --metadata-file "metadata.json" \
- --jar "/usr/src/app/app.jar" \
+ --jar "target/flex-template-getting-started-1.0.jar" \
  --env FLEX_TEMPLATE_JAVA_MAIN_CLASS="com.example.dataflow.FlexTemplateGettingStarted"
